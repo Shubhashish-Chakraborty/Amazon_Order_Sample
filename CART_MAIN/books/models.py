03a6bd68b72eb0d7decefd7e245e65e87999e0a9
Shubhashish-Chakraborty <shubhashish147@gmail.com>
2024-06-05 11:13:07 +0530

    Worked on DATABASE!!

--- CART_MAIN/books/models.py
+++ CART_MAIN/books/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Order_Book(models.Model):
+
+
+    Books_Available = [
+
+        ("B1" , "The Power of your subconscious mind"),
+        ("B2" , "Don't Believe everything you think"),
+        ("B3" , "The Alchemist"),
+        ("B4" , "The Silence Patient"),
+        ("B5" , "Focus on what matters"),
+        ("B6" , "11 Rules for Life"),
+        ("B7" , "Lucifer was Innocent"),
+
+    ]
+
+    Order_Placed = models.DateTimeField(default=timezone.now)
+
+
+    Select_Book_and_Place_Order = models.CharField(max_length=2 , choices=Books_Available , default="B7")
