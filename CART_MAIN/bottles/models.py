1045f353a0518600df598a4028c25cc996395f35
Shubhashish-Chakraborty <shubhashish147@gmail.com>
2024-06-08 11:56:24 +0530

    Worked on Bottle models

--- CART_MAIN/bottles/models.py
+++ CART_MAIN/bottles/models.py
@@ -1,3 +1,52 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Order_Bottle(models.Model):
+
+
+    Bottles_Available = [
+
+        ("Bot1" , "Bold Fit Water Bottles"),
+        ("Bot2" , "Cello Puro WaterBottles"),
+        ("Bot3" , "Jugte Bottles"),
+        ("Bot4" , "SpeedEX Water Bottles"),
+
+
+    ]
+
+    Qauntity_Available = [
+
+        ("01" , "1"),
+        ("02" , "2"),
+        ("03" , "3"),
+        ("04" , "4"),
+        ("05" , "5"),
+        ("06" , "6"),
+        ("07" , "7"),
+        ("08" , "8"),
+
+
+    ]
+
+    Price_Variant = [
+
+        ("$35" , "$35"),
+        ("$48" , "$48"),
+        ("$56" , "$56"),
+        ("$89" , "$89"),
+
+
+    ]
+
+    Order_Placed = models.DateTimeField(default=timezone.now)
+
+
+    Select_Bottle = models.CharField(max_length=4 , choices=Bottles_Available , default="Bot1")
+    Select_Quantity= models.CharField(max_length=2 , choices=Qauntity_Available , default="01")
+    Select_Price = models.CharField(max_length=3 , choices=Price_Variant , default="$48")
+
+    Confirm_bottle = models.ImageField(upload_to="bottle_imgs/" , default="") 
+
+    def __str__(self):
+        return self.Select_Bottle
