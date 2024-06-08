a165e5c9df505e687408b71a3f4ace1bb803cc57
Shubhashish-Chakraborty <shubhashish147@gmail.com>
2024-06-08 12:04:17 +0530

    ORDER BOTTLES - CHECKPOINT 4

--- CART_MAIN/bottles/views.py
+++ CART_MAIN/bottles/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
-
+from .models import Order_Bottle
 
 def home(request):
+    
+    BOTTLES = Order_Bottle.objects.all()
 
-    return render(request, 'bottles_temp/bottle.html')
+    return render(request, 'bottles_temp/bottle.html' , {'BOTTLES': BOTTLES})
